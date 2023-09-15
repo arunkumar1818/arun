21753b63d56d593b1469bba5f38c89fd4762b24e
arunkumar1818 <144580171+arunkumar1818@users.noreply.github.com>
2023-09-15 06:54:27 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,30 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        `<span>Hi, I'm அருண்குமார் AK</span> <br>I'm 22 Years Old <br> ROLE : SYSTEM ADMINISTRATOR 
+      <br><br><br>
+   			<ul class="profile">
+   				
+   				<li>
+   					<strong>Birth Date:</strong>
+   					<span>july 27, 2000</span>
+   				</li>
+   				<li>
+   					<strong>student:</strong>
+   					<span> MCA Guru Nanak College</span>
+   				</li>
+   				<li>
+   					<strong>Website:</strong>
+   					<span>www.arun.com</span>
+   				</li>
+   				<li>
+   					<strong>Email:</strong>
+   					<span>arunat7305@gmail.com</span>
+   				</li>
+
+   			</ul> <!-- /info-list -->
+        `],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
